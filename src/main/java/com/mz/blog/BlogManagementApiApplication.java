336f496328dfea456c04df1357d9e86ff2a871c3
Murtaza <murtazamaimoon73@gmail.com>
2026-02-09 20:53:49 +0530

    Initial commit Project setup

---
+++ src/main/java/com/mz/blog/BlogManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.mz.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogManagementApiApplication.class, args);
+    }
+
+}
